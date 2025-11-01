a48583c90ee4921ae696370e1fda04be81f473d9
Kiet Le <ktlbusiness11@gmail.com>
2025-10-31 22:45:25 -0400

    Theta: Paramaterize and Add comments

--- rtl/theta_step.sv
+++ rtl/theta_step.sv
@@ -1,19 +1,30 @@
-module theta_step (
-    input [4:0][4:0][63:0] state_array_in,
-    output [4:0][4:0][63:0] state_array_out
+/*
+ * Module Name: theta_step
+ * Author: Kiet Le
+ * Description: Theta in Keccak is a diffusion step that mixes each bit with a parity of neighboring columns.
+ * NOTE: Purely combinational so far. Can be pipelined for higher clock speed if needed.
+ */
+
+module theta_step #(
+    parameter integer LANE_SIZE     = 64,
+    parameter integer COL_SIZE      = 5,
+    parameter integer ROW_SIZE      = 5;
+)(
+    input   [ROW_SIZE-1:0][COL_SIZE-1:0][LANE_SIZE-1:0] state_array_in,
+    output  [ROW_SIZE-1:0][COL_SIZE-1:0][LANE_SIZE-1:0] state_array_out
 );
-
     // Column Parity Wires (Algorithm 1: C matrix in FIPS202)
-    // 5x64 grid array (each entry is a parity of the corresponding column)
-    wire [4:0][63:0] C;
+    wire [4:0][LANE_SIZE-1:0] C;
 
-    wire [4:0][63:0] D;
+    // Deltas Wires (Algorithm 1: D matrix in FIPS202)
+    wire [4:0][LANE_SIZE-1:0] D;
 
     // Get Parities of each column
-    // From FIPS202: C[x, z] = A[x, 0, z] ⊕ A[x, 1, z] ⊕ A[x, 2, z] ⊕ A[x, 3, z] ⊕ A[x, 4, z]
+    // - From FIPS202: C[x, z] = A[x, 0, z] ⊕ A[x, 1, z] ⊕ A[x, 2, z] ⊕ A[x, 3, z] ⊕ A[x, 4, z]
+    // - 5x64 grid array (each entry is a parity of the corresponding column)
     genvar x;
     generate
-        for (x = 0; x<5; x = x + 1) begin : compute_C
+        for (x = 0; x<ROW_SIZE; x = x + 1) begin : compute_C
             assign C[x] =       state_array_in[x][0] ^
                                 state_array_in[x][1] ^
                                 state_array_in[x][2] ^
@@ -22,26 +33,32 @@ module theta_step (
         end
     endgenerate
 
-    // Calculate D[x] to mix neighboring column parities with rotation
-    // From FIPS202: D[x, z] = C[(x-1) mod 5, z] ⊕ C[(x+1) mod 5, (z – 1) mod w].
+    /* Calculate D[x] to mix neighboring column parities with rotation
+     * - From FIPS202: D[x, z] = C[(x-1) mod 5, z] ⊕ C[(x+1) mod 5, (z – 1) mod w]
+     * - For each column, compute a “delta” by XORing the parity of the previous ...
+     *   column (mod 5) with a rotated parity of the next column.
+     */
     generate
-        for (x = 0; x<5; x = x + 1) begin : compute_D
+        for (x = 0; x<ROW_SIZE; x = x + 1) begin : compute_D
             // Efficient Way to implement the modulo in this case
             wire [2:0] xm1 = (x==0) ? 4 : x - 1; // x - 1 modulo 5
             wire [2:0] xp1 = (x==4) ? 0 : x + 1; // x + 1 modulo 5
 
             // C[x-1] XOR {Rotated C[x+1]}
             // Doing the XOR operation with each lane of the C array
-            assign D[x] = C[xm1] ^ {C[xp1][62:0], C[xp1][63]};
+            assign D[x] = C[xm1] ^ {C[xp1][LANE_SIZE-2:0], C[xp1][LANE_SIZE-1]};
         end
     endgenerate
 
-    // Compute final state array for theta step
-    // From FIPS202: A′[x, y, z] = A[x, y, z] ⊕ D[x, z]
+    /*
+     * Compute final state array for theta step
+     * - From FIPS202: A′[x, y, z] = A[x, y, z] ⊕ D[x, z]
+     * - XOR this delta into each bit of the column
+     */
     genvar y;
     generate
-        for (x = 0; x<5; x = x + 1) begin
-            for (y = 0; y<5; y = y + 1) begin
+        for (x = 0; x<ROW_SIZE; x = x + 1) begin
+            for (y = 0; y<COL_SIZE; y = y + 1) begin
                 assign state_array_out[x][y] = state_array_in[x][y] ^ D[x];
             end
         end
